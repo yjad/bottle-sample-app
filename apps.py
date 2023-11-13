6874a87e93580580c535cbb11ea527bae3ebef2c
Yahia Jad <yahia.jad@gmail.com>
2023-11-13 07:50:16 +0200

    initial

---
+++ apps.py
@@ -0,0 +1,9 @@
+import bottle
+import routes
+
+app = routes.app
+
+if __name__ == '__main__':
+    # this setting is running for development.
+    # bottle.run(app=app, host='0.0.0.0',port=8080, reloader=True, debug=True)
+    bottle.run(app=app, host='localhost',port=8080, reloader=True, debug=True)
